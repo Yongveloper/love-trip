9b7c0c064b7a7c8dfb2eb2b1745a16b196e90850
yongveloper <dev_yong@kakao.com>
2025-01-30 18:28:41 +0900

    feat: 호텔 리스트 페이지 및 테스트 페이지 추가, 스타일 시트 제거

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,6 @@ import { createRoot } from 'react-dom/client';
 import { Global } from '@emotion/react';
 
 import App from './App.tsx';
-import './index.css';
 import globalStyles from './styles/globalStyles';
 
 createRoot(document.getElementById('root')!).render(
